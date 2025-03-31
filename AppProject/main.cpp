d4e3d273fda6a313086565d8e2748a980ae5cd18
tnakaicode <tnakaicode@gmail.com>
2025-03-31 11:37:39 +0900

    launch env

--- AppProject/main.cpp
+++ AppProject/main.cpp
@@ -1,13 +1,24 @@
+#include <iostream>
 #include <BRepPrimAPI_MakeBox.hxx>
 #include <TopoDS_Shape.hxx>
 #include <BRepTools.hxx>
-#include <iostream>
+#include <OSD_Environment.hxx>
+#include <gp_Pnt.hxx>
 
 int main() {
+    // OpenCASCADEのログをファイルにリダイレクト
+    OSD_Environment logEnvOut("CSF_DefaultStdOut", "log.txt");
+    OSD_Environment logEnvErr("CSF_DefaultStdErr", "error_log.txt");
+
     std::cout << "OpenCASCADE Example" << std::endl;
+
+    // 点を作成
+    gp_Pnt point(1.0, 2.0, 3.0);
+    std::cout << "Point coordinates: (" << point.X() << ", " << point.Y() << ", " << point.Z() << ")" << std::endl;
 
     // ボックスを作成
     TopoDS_Shape box = BRepPrimAPI_MakeBox(100.0, 50.0, 30.0).Shape();
+    std::cout << "After creating the box" << std::endl;
 
     // ボックスをファイルに保存
     if (BRepTools::Write(box, "box.brep")) {
