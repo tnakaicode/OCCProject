a0ed4430120ca1e9a4c9d799b662daf64e48cd1e
tnakaicode <tnakaicode@gmail.com>
2025-03-31 10:55:36 +0900

    opencascade

--- AppProject/main.cpp
+++ AppProject/main.cpp
@@ -1,6 +1,20 @@
+#include <BRepPrimAPI_MakeBox.hxx>
+#include <TopoDS_Shape.hxx>
+#include <BRepTools.hxx>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "OpenCASCADE Example" << std::endl;
+
+    // ボックスを作成
+    TopoDS_Shape box = BRepPrimAPI_MakeBox(100.0, 50.0, 30.0).Shape();
+
+    // ボックスをファイルに保存
+    if (BRepTools::Write(box, "box.brep")) {
+        std::cout << "Box shape saved to 'box.brep'" << std::endl;
+    } else {
+        std::cerr << "Failed to save box shape" << std::endl;
+    }
+
     return 0;
 }
