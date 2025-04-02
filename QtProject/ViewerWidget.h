4c60c5c080099bb7fbf23eb34475926b0eb234c0
tnakaicode <tnakaicode@gmail.com>
2025-04-02 14:54:53 +0900

    foirmat

--- QtProject/ViewerWidget.h
+++ QtProject/ViewerWidget.h
@@ -10,28 +10,29 @@
 #include <BRepPrimAPI_MakeBox.hxx>
 #include <TopoDS_Shape.hxx>
 
-class ViewerWidget : public QWidget {
+class ViewerWidget : public QWidget
+{
     Q_OBJECT
 
 public:
-    explicit ViewerWidget(QWidget* parent = nullptr);
+    explicit ViewerWidget(QWidget *parent = nullptr);
     ~ViewerWidget();
 
     // ボックスや他の形状を表示するためのメソッド
-    void displayShape(const TopoDS_Shape& shape, double transparency = 0.0);
+    void displayShape(const TopoDS_Shape &shape, double transparency = 0.0);
 
 protected:
-    void paintEvent(QPaintEvent* event) override;
-    void resizeEvent(QResizeEvent* event) override;
-    void mousePressEvent(QMouseEvent* event) override;
-    void mouseMoveEvent(QMouseEvent* event) override;
-    void wheelEvent(QWheelEvent* event) override;
+    void paintEvent(QPaintEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void wheelEvent(QWheelEvent *event) override;
 
 private:
     Handle(V3d_Viewer) m_viewer;
-    Handle(V3d_View) m_view; // OpenCASCADE のビュー
+    Handle(V3d_View) m_view;                  // OpenCASCADE のビュー
     Handle(AIS_InteractiveContext) m_context; // OpenCASCADE の描画コンテキスト
-    QPoint m_lastMousePos; // マウスの最後の位置
+    QPoint m_lastMousePos;                    // マウスの最後の位置
 };
 
 #endif // VIEWERWIDGET_H
--- QtProject/main.cpp
+++ QtProject/main.cpp
@@ -2,7 +2,8 @@
 #include <QApplication>
 #include <BRepPrimAPI_MakeBox.hxx> // OpenCASCADE のボックス作成用クラス
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication app(argc, argv);
 
     ViewerWidget viewer;
